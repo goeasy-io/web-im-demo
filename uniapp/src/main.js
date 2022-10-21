d2895abf21bb9e9bab55eb43f1b70d24d49d407b
BettyWbb <betty@goeasy.io>
2022-10-21 18:12:17 +0800

    将uniapp项目移到src目录下，以便ci

--- uniapp/src/main.js
+++ uniapp/src/main.js
@@ -9,25 +9,25 @@ const app = new Vue({
 });
 app.$mount();
 const goEasy = GoEasy.getInstance({
-	host:'hangzhou.goeasy.io',//应用所在的区域地址: 【hangzhou.goeasy.io |singapore.goeasy.io】
-	appkey:'BC-xxxx',	// common key,
-    modules:['im'],
-	// true表示支持通知栏提醒，false则表示不需要通知栏提醒
+    host: 'hangzhou.goeasy.io',//应用所在的区域地址: 【hangzhou.goeasy.io |singapore.goeasy.io】
+    appkey: 'BC-xxxx',	// common key,
+    modules: ['im'],
+    // true表示支持通知栏提醒，false则表示不需要通知栏提醒
     allowNotification: true //仅有效于app,小程序和H5将会被自动忽略
 });
 
 goEasy.onClickNotification((message) => {
-	let currentUrl;
-	const routes = getCurrentPages();
+    let currentUrl;
+    const routes = getCurrentPages();
 
-	if ( routes &&  routes.length ) {
-		const curRoute = routes[routes.length - 1].route;
-		const curParam = routes[routes.length - 1].options;
-		currentUrl = '/' + curRoute + `?to=${curParam.to}`;
-	}
+    if (routes && routes.length) {
+        const curRoute = routes[routes.length - 1].route;
+        const curParam = routes[routes.length - 1].options;
+        currentUrl = '/' + curRoute + `?to=${curParam.to}`;
+    }
 
     let newUrl;
-    switch(message.toType) {
+    switch (message.toType) {
         case GoEasy.IM_SCENE.PRIVATE:
             newUrl = '/pages/chat/privateChat/privateChat?to=' + message.senderId;
             break;
