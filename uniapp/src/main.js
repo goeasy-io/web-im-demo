f0d326164a52d71bbff0abecb072a959896acbff
BettyWbb <betty@goeasy.io>
2022-11-28 15:47:16 +0800

    uniapp更新SDK

--- uniapp/src/main.js
+++ uniapp/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App';
-import GoEasy from './lib/goeasy-2.5.11.min';
+import GoEasy from './lib/goeasy-2.5.12.min';
 
 Vue.config.productionTip = false;
 App.mpType = 'app';
