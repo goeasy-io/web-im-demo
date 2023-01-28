5f1125eb08bab24ef664429de709954965dcfdc9
fcb <chuanbao@uwantsoft.com>
2023-01-28 15:02:18 +0800

    1.修改测试node操作ftp服务的ftp.js

--- ftp.js
+++ ftp.js
@@ -1,7 +1,11 @@
+const fs = require('fs');
 const ftp = require('ftp');
+const path = require('path');
+
 const client = new ftp();
 client.on('ready', () => {
     console.log('ftp client is ready');
+    test();
 });
 client.connect({
     host: '77.74.54.201',
@@ -10,3 +14,73 @@ client.connect({
     password: 'jiacai',
     keepalive: 1000
 });
+
+// 查询文件列表
+function list(dirpath) {
+    return new Promise((resolve, reject) => {
+        client.list((err, files) => {
+            resolve({
+                err: err,
+                files: files
+            });
+        })
+    });
+}
+
+//将文件上传到ftp目标地址
+async function put(currentFile,targetFilePath){
+    const dirPath = path.dirname(targetFilePath);
+    const fileName = path.basename(targetFilePath);
+    const rs = fs.createReadStream(currentFile);
+    let {err : ea,dir} = await cwd(dirPath);//此处应对err做处理
+    if(ea){
+        return Promise.resolve({err : ea});
+    }
+    return new Promise((resolve,reject)=>{
+        client.put(rs,fileName,(err)=>{
+            resolve({err : err});
+        })
+    });
+}
+//切换目录
+function cwd(dirpath){
+    return new Promise((resolve,reject)=>{
+        client.cwd(dirpath,(err,dir)=>{
+            resolve({err : err,dir : dir});
+        })
+    });
+}
+//下载文件
+async function get(filePath){
+    const dirPath = path.dirname(filePath);
+    const fileName = path.basename(filePath);
+    let {err : ea,dir} = await cwd(dirPath);
+    return new Promise((resolve,reject)=>{
+        client.get(fileName,(err,rs)=>{
+            let ws = fs.createWriteStream(fileName);
+            rs.pipe(ws);
+            resolve({err : err});
+        });
+    });
+}
+
+//获取所有文件列表
+async function test() {
+    let {
+        err,
+        files
+    } = await list();
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log(`获得文件列表:` + files.length);
+    console.log(files);
+    //文件上传
+    let {err : eb} = await put('index.html', '/index.html');
+    if(eb){
+        console.log(eb);
+        return;
+    }
+    console.log('文件上传成功')
+}
