c458a736c535d811dea7f89169a7d4e02a8965c3
build-ci <build-ci@goeasy.io>
2023-01-28 20:50:51 +0800

    使用云虚拟主机的配置

--- ftp.js
+++ ftp.js
@@ -3,12 +3,12 @@ const packageJson = require('./package.json');
 const ftpDeploy = new FtpDeploy();
 
 var config = {
-    // user: "hyu7385950001",
-    // password: "Uwant123",
-    // host: "hyu7385950001.my3w.com",
-    user: "jiacai",
-    password: "jiacai",
-    host: "77.74.54.201",
+    user: "hyu7385950001",
+    password: "Uwant123",
+    host: "hyu7385950001.my3w.com",
+    // user: "jiacai",
+    // password: "jiacai",
+    // host: "77.74.54.201",
     port: 21,
     localRoot: "./test",
     remoteRoot: "/htdocs/docs/" + packageJson.version,
