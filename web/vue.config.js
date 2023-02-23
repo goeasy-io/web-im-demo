3b511dbaf47a9871176bc5a6c5c777069d6d97d1
BettyWbb <betty@goeasy.io>
2023-02-23 17:28:11 +0800

    更新vue-cli

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,9 +1,10 @@
-module.exports = {
-  lintOnSave : false,
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: '/',
   //只有Https才能使用录音
   devServer : {
     https : true,
     port : 9004
   }
-}
+})
