dde7598fced2048b21a5c3cf59a98ca5a7b99121
BettyWbb <betty@goeasy.io>
2023-04-12 11:57:36 +0800

    字体

--- uniapp/src/App.vue
+++ uniapp/src/App.vue
@@ -14,12 +14,12 @@
 </script>
 
 <style>
+  @import url('./static/style/chatInterface.css');
   * {
-    font-family: "HanHei SC",PingHei,"PingFang SC","Helvetica Neue",Helvetica,Arial,"Microsoft Yahei","Hiragino Sans GB","Heiti SC","WenQuanYi Micro Hei",sans-serif;;
+    font-family: "HanHei SC",PingHei,"PingFang SC","Helvetica Neue",Helvetica,Arial,"Microsoft Yahei","Hiragino Sans GB","Heiti SC","WenQuanYi Micro Hei",sans-serif;
     font-style: normal;
     font-weight: normal;
   }
-  @import url('./static/style/chatInterface.css');
   /* 隐藏滚动条 */
   uni-scroll-view .uni-scroll-view::-webkit-scrollbar {
     display: none;
